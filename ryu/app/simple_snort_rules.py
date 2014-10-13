e915ed462de0de1bea8e36bf64ac51072da33ce0
Ramprakash Ram Mohan <rmohan@sjc-rmohan-mba.local>
2014-10-13 00:19:36 -0700

    Added new classes for parser and rules. The IDS would typically spawn 2 instances of the parser - one for deep probing and another one for light probing - each with its own list of rules.

--- ryu/app/simple_snort_rules.py
+++ ryu/app/simple_snort_rules.py
@@ -7,47 +7,94 @@ import re
 from pyparsing import *
 
 
-#from ryu.base import app_manager
-#from ryu.controller import ofp_event
-#from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
-#from ryu.controller.handler import set_ev_cls
-#from ryu.ofproto import ofproto_v1_3
-#from ryu.lib.packet import packet
-#from ryu.lib.packet import ethernet
-
-# TODO: port numbers do not support range (rule in line 2427, 2574, 2942,
-# 2945-2961, 2978-2989, 3013) and list (rule in # line 2424). 
-# Need to tweak the grammar a bit to get this to work
-
 ipAddress = Combine(Word(nums) + ('.' + Word(nums))*3)
 hexint = Word(hexnums,exact=2)
 specialchars = Word(alphanums+"."+"["+"]"+"$"+":"+"_"+"-"+">"+"\""+"("+")" \
-                             +"\\"+";"+","+" "+"'"+"/"+"?"+"|"+"=")
+                             +"\\"+";"+","+" "+"'"+"/"+"?"+"|"+"="+"*"+"!" \
+                             +"{"+"}"+"<")
 macAddress = Combine(hexint + (':'+hexint)*5)
 identifier = Word(alphas+"_", exact=1) + Optional(Word(alphanums+"_"))
 envvar     = Combine("$" + identifier)
 portnum    = Word(nums) | Word(alphas) | envvar 
+
+# action: alert, drop etc.
+action     = Word(alphas) 
+# src_ip/dst_ip: IP, env var like $HOME_NET or rserved wild-card like 'any'
+src_ip     = (ipAddress | envvar | "any")
+dst_ip     = (ipAddress | envvar | "any")
+# proto: could be udp, tcp or a decimal number like 65535 (not in hex yet)
+proto      = (Word(alphas) | Word(nums))
+# options: A world between '(' & ')'
 options    = Combine("(" + Optional(specialchars))
 
-#Snort rules take the form:
-#              action proto src_ip src_port direction dst_ip dst_port (options)
-snort_grammar = Word(alphas) + (Word(alphas) | Word(nums)) + \
-               (ipAddress | envvar | "any") + portnum + "->" + \
-               (ipAddress | envvar | "any") + portnum + options
+class Rule:
+    def __init__(self, action, proto, src_ip, dst_ip, src_port, dst_port,
+            options):
+        self.action = action
+        self.proto  = proto
+        self.src_ip = src_ip
+        self.dst_ip = dst_ip
+        self.src_port = src_port
+        self.dst_port = dst_port
+        self.options  = options
+
+class SnortParser:
+    # Should be a singleton class - One instance is enough for the lifetime of
+    # the IDS application
+
+    # TODO: port numbers do not support range (rule in line 2427, 2574, 2942,
+    # 2945-2961, 2978-2989, 3013) and list (rule in # line 2424). 
+    # Need to tweak the grammar a bit to get this to work
+
+    def __init__(self, rule_file="./snort.rules"):
+        """
+        " Can be called with one file for light-probing rules and another for
+        " deep probing rules - so not exactly a singleton as mentioned above
+        """
+
+        self.rules = []
+
+        # Snort rules take the form:
+        #   action proto src_ip src_port direction dst_ip dst_port (options)
+        snort_grammar = action + proto + src_ip + portnum + "->" + \
+               dst_ip + portnum + options
+
+        fp_rules = open(rule_file, 'r')
+        index = 0
+        for rule in fp_rules:
+            # Ignore comments
+            if ((re.search('^ *#', rule) == None) and 
+                (re.search('^ *$', rule) == None)):
+                try:
+                    tokens = snort_grammar.parseString(rule)
+                    self.rules.append(Rule(tokens[0], tokens[1], tokens[2],
+                            tokens[5], tokens[3], tokens[6], tokens[7]))
+                    index = index + 1
+                except:
+                    print "\nSyntax error at line %d in rule: %s" % (index, rule)
+                    traceback.print_exc(file=sys.stdout)
+                    break
+
+        self.num_rules = index;
+        print "Total number of rules: %d : %d" % (self.num_rules, len(self.rules))
+
+    def dumpRules(self):
+        index = 0;
+        while (index < len(self.rules)):
+            rule = self.rules[index]
+            print "\n%d: %s %s %s -> %s %s %s" % (index, rule.action,
+                    rule.src_ip, rule.src_port, rule.dst_ip, rule.dst_port,
+                    rule.options)
+            index = index + 1
+
+    def getRules(self):
+        """ 
+        " API for IDS application to get the rules configured int the system
+        """
+        # TODO
 
 
-rules_file = open('./snort.rules', 'r')
-line = 0
-for rule in rules_file:
-    line = line + 1
-    # Ignore comments
-    if ((re.search('^ *#', rule) == None) and 
-            (re.search('^ *$', rule) == None)):
-        try:
-            tokens = snort_grammar.parseString(rule)
-            print "Tokens: %s" % tokens
-        except:
-            print
-            print "\nSyntax error at line %d in rule: %s" % (line, rule)
-            traceback.print_exc(file=sys.stdout)
-            break
+# Testing
+parser = SnortParser()
+parser.dumpRules()
+
