96dce072f60e70b54b6de4da8c272b2977bda324
Nav V <vnav0813@gmail.com>
2014-10-29 05:49:19 -0700

    Adding echo test scripts

---
+++ ryu/app/AdaptiveIDS/adaptiveids/echo-scripts/echo_server.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+"""
+A simple echo server
+"""
+
+import socket
+
+host = ''
+port = 50000
+backlog = 5
+size = 1024
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host,port))
+s.listen(backlog)
+while 1:
+    client, address = s.accept()
+    data = client.recv(size)
+    client.close()
